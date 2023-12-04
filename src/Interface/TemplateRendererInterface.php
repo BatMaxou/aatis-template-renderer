58d3d34ed1ade2c5e6f3cfb28f0894a59ca0ad4f
Maxime <maxime@maximebatista.lycee@gmail.com>
2023-12-04 23:55:06 +0100

    :bookmark: Php template renderer

--- src/Interface/TemplateRendererInterface.php
+++ src/Interface/TemplateRendererInterface.php
@@ -4,5 +4,10 @@ namespace Aatis\TemplateRenderer\Interface;
 
 interface TemplateRendererInterface
 {
+    public const EXTENSION = '';
+
+    /**
+     * @param array<string, mixed> $data
+     */
     public function render(string $template, array $data = []): void;
 }
